00ef54a6531f1ecbb097724d3c1bf3d054e07b89
Brit <Brit@DESKTOP-BFORP22>
2022-06-01 20:52:41 -0700

    Add project files.

---
+++ Constants/Program.cs
@@ -0,0 +1,8 @@
+﻿//constants = immutable values which are known at compile time
+//            and do not change for the life of the program
+
+const double pi = 3.14159;
+
+Console.WriteLine(pi);
+
+Console.ReadKey();
